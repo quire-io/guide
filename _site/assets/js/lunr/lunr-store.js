8052e3e6002c89f39925336c9e81e2ffcc6f7130
Jerry Chen <b80203@gmail.com>
2019-05-31 18:24:45 +0800

    init

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"The bread and butter of any theme. Below you’ll find the layouts included with Minimal Mistakes, what they look like and the type of content they’ve been built for. Default layout The base layout all other layouts inherit from. There’s not much to this layout apart from pulling in several...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/docs/quick-start-guide/",
+        "teaser":null},]
