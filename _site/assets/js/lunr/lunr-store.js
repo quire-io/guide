acfdaeb73c667ff1026f5421b519156e17335ad0
Jerry Chen <b80203@gmail.com>
2019-06-24 18:09:53 +0800

    fix disqus

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,31 @@
 var store = [{
         "title": "Welcome to Jekyll!",
-        "excerpt":"Overview &gt; User Interface User Interface Your Quire workspace is divided into 3 main sections: Sidebar, Main Panel, and Detail Panel. Sidebar: access to My Tasks, Bookmarks, Smart Folders, Recent, Organizations, Projects, and Members. Main Panel: display the tasks of the project that you are currently viewing. Access to List...","categories": ["user","interface"],
+        "excerpt":"User Interface Your Quire workspace is divided into 3 main sections: Sidebar, Main Panel, and Detail Panel. Sidebar: access to My Tasks, Bookmarks, Smart Folders, Recent, Organizations, Projects, and Members. Main Panel: display the tasks of the project that you are currently viewing. Access to List view, Board view, Overview,...","categories": ["user","interface"],
         "tags": [],
         "url": "http://localhost:4000/guide/user-interface/",
         "teaser":null},{
         "title": "Welcome to Jekyll!",
-        "excerpt":"Overview &gt; Organization, Project and Task Organization, Project and Task With a Quire account, you can create or access one or more organizations to collaborate and work on projects with different team members. Each organization has its own set of projects and projects consist of tasks and subtasks. For example,...","categories": ["organization","project","task"],
+        "excerpt":"Organization, Project and Task With a Quire account, you can create or access one or more organizations to collaborate and work on projects with different team members. Each organization has its own set of projects and projects consist of tasks and subtasks. For example, you are the CEO of your...","categories": ["organization","project","task"],
         "tags": [],
         "url": "http://localhost:4000/guide/organization-project-task/",
+        "teaser":null},{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Members There are two types of member levels: project level and organization level. If you are a member at the organization level, you would have access to all the projects underneath. If you are a member at the project level, you would only have access to the projects that you...","categories": ["members"],
+        "tags": [],
+        "url": "http://localhost:4000/guide/members/",
+        "teaser":null},{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Project Views With different positions in a team or different stages in a project, you might need to view your projects in a different way. For example, as a team leader, you probably will need the board view more than your team members to visually track the overall project flow...","categories": ["project","views"],
+        "tags": [],
+        "url": "http://localhost:4000/guide/project-view/",
+        "teaser":null},{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Navigating Quire Project/Organization Dropdown Menu Click on the arrow beside the project or organization name to bring out more options. Right-click on task(s) Right-click on a task to bring out the context menu. Note: Only when hovering over a task will show the status circle, date/ assignee icon, and +...","categories": ["navigation"],
+        "tags": [],
+        "url": "http://localhost:4000/guide/navigate-quire/",
+        "teaser":null},{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Mobile Apps Quire is also available on mobile apps, making it easier for you to manage projects wherever and whenever you want. iOS Download Quire iOS app in the App Store. Android Download Quire Android app in Google Play. If you are located in China or don’t have access to...","categories": ["mobile","apps"],
+        "tags": [],
+        "url": "http://localhost:4000/guide/mobile-apps/",
         "teaser":null},]
