ac9123f62b7ec5508c00b44f8f51678c31d418c3
Jerry Chen <b80203@gmail.com>
2019-06-24 17:10:14 +0800

    update css

--- assets/js/custom-footer.js
+++ assets/js/custom-footer.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
   
   $('.sidebar > .nav__list > input').change(function() {
-    console.log('check' + this.checked);
     $('body').toggleClass('sticky-sidebar', this.checked);
   });
 });
