ce92fd3addc1c60d4fbf2949934bc75594e5fef4
Jerry Chen <b80203@gmail.com>
2019-07-31 14:18:01 +0800

    Fixed covered by header for scrolling to hash tag

--- assets/js/custom-footer.js
+++ assets/js/custom-footer.js
@@ -4,4 +4,20 @@ $(document).ready(function() {
   $('.sidebar > .nav__list > input').change(function() {
     $('body').toggleClass('sticky-sidebar', this.checked);
   });
+
+  $('.toc__menu').on('click', 'a', function(e) {
+  	var idx = this.href.indexOf("#");
+	var hash = idx != -1 ? this.href.substring(idx) : null;
+
+	if (hash) {
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $(hash).offset().top - 64/*header height*/
+		}, 500);
+
+		e.preventDefault(); 
+		return false;
+	}
+
+	return true;
+  });
 });
